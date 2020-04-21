da0106d2577cfbe3cd8e157b89a7771f706b0789
Luke Espinosa <lucaslaneespinosa@gmail.com>
2020-04-21 00:18:20 -0400

    added some notes and test iterator

--- bob_bifford_bot.py
+++ bob_bifford_bot.py
@@ -5,9 +5,15 @@ def getFumbblTournamentNames(fumbblUrl):
     res.raise_for_status()
 
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    elems = soup.select('body > div.contentwrapper > div.pagecontent > div > div.gamescontent > div:nth-child(25) > a')
-    return elems[0].text
+    ## TODO: build iterator to go over all returned game names. Store the ones we want in a list
+    for x in range (0, 50):
+        try:
+            elems = soup.select('body > div.contentwrapper > div.pagecontent > div > div.gamescontent > div:nth-child('') > a')
+            return elems[0].text
+        except IndexError:
+            print('no game with that id')
 
+    ## TODO: take the list and then filter out ones that do not match league name
 
 current_tournament_list = getFumbblTournamentNames('https://fumbbl.com/p/games')
 print('The current Underground tournaments are: ' + current_tournament_list)
